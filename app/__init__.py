90cd6571c54aa74d6a8b7e45355900c96d4cf755
lolThinked <fabianhopland@gmail.com>
2020-07-24 10:17:58 +0200

    MAJOR UPDATE(Transfer to bigger design language "Flask")

---
+++ app/__init__.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, jsonify, url_for, send_from_directory, request
+from flask_login import LoginManager
+import json
+import os
+
+from threading import Thread
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
+
+from base64 import decodestring, decodebytes
+import base64
+
+
+
+app = Flask(__name__)
+
+from app import routes
+login_manager = LoginManager()
+#login_manager.init_app(app)
+
+print(app)
+#print(os.getcwd())
+
+
+overView = {}
+idList = []
+
+
+
+def initializeMemoryData():
+    print("[INITILIAZING] - Local data to Memory (overview, idlist)")
+    filesize = os.path.getsize("Saved/overView.json")
+    with open("Saved/overView.json", "r") as f:
+        if(filesize !=0):
+            overView = json.load(f)
+            return overView
+    print("[OVERVIEW] - Complete!")
+
+def initializeIDList():
+    filesize = os.path.getsize("Saved/idList.txt")
+    with open("Saved/idList.txt", "r") as inList:
+        #listIDS = list(inList)
+        listIDS = [line.rstrip('\n') for line in inList]
+        #print(listIDS)
+    print("[IDLIST] - Complete!")
+    print("[INITIALIZATION] - Done!")
+    return listIDS
+
+
+overView = initializeMemoryData()
+idList = initializeIDList()
+#print(idList)
+#print(overView)
+
+    
+
+##############                   For Compiling Only               ###########################
+if __name__ =='__main__':
+    app.run(host='0.0.0.0',port=5000)
+    #app.run(debug=True)
+    #threading.Thread(target=app.run).start()
+##############                   For Compiling Only               ###########################
+
+
+app.run(host='0.0.0.0',port=5000)
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
+from app import app
 from flask import Flask, render_template, jsonify, url_for, send_from_directory, request
-from flask_login import LoginManager
+#from flask_login import LoginManager
 import json
 import os
 
@@ -10,50 +11,14 @@ from selenium.webdriver.chrome.options import Options
 from base64 import decodestring, decodebytes
 import base64
 
-
-
-app = Flask(__name__)
-
-from app import routes
-login_manager = LoginManager()
-#login_manager.init_app(app)
-
-
-
-overView = {}
-idList = []
-
-
-
-def initializeMemoryData():
-    print("[INITILIAZING] - Local data to Memory (overview, idlist)")
-    filesize = os.path.getsize("Saved/overView.json")
-    with open("Saved/overView.json", "r") as f:
-        if(filesize !=0):
-            overView = json.load(f)
-            return overView
-    print("[OVERVIEW] - Complete!")
-
-def initializeIDList():
-    filesize = os.path.getsize("Saved/idList.txt")
-    with open("Saved/idList.txt", "r") as inList:
-        #listIDS = list(inList)
-        listIDS = [line.rstrip('\n') for line in inList]
-        #print(listIDS)
-    print("[IDLIST] - Complete!")
-    print("[INITIALIZATION] - Done!")
-    return listIDS
-
-
-overView = initializeMemoryData()
-idList = initializeIDList()
-#print(idList)
-#print(overView)
+#from app import overview, idList
 
 #HOME DEFAULT ROUTE
 @app.route("/")
 @app.route("/index")
 def home():
+    #print(os.getcwd())
+    #print(os.path.exists("templates/index_flaskpage.html"))
     return render_template("index_flaskpage.html", obj=0)
 
 @app.route("/save", methods=["post"])
@@ -215,14 +180,3 @@ def getScreenshotFromID(id):
     with open(r"static/images/IDImages/"+id+".png", 'wb') as f:
         f.write(canvas_png)
     print("[SCREENSHOT] - Done! ('static/images/IDImages/"+id+".png')")
-    
-
-##############                   For Compiling Only               ###########################
-if __name__ =='__main__':
-    app.run(host='0.0.0.0',port=5000)
-    #app.run(debug=True)
-    #threading.Thread(target=app.run).start()
-##############                   For Compiling Only               ###########################
-
-
-
