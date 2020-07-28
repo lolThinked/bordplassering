142d5c423539da4a07129c7d023508dcb6e9e038
Fabian Hauk Hopland <fabianhopland@gmail.com>
2020-07-28 02:21:58 +0200

    Beggining of classes and objects in javascriptBeggining of classes and objects in javascript

--- app/static/js/PersonClass.js
+++ app/static/js/PersonClass.js
@@ -1,8 +1,35 @@
 class Person{
-    constructor(firstName, surName, age, gender){
-        this.firstName = firstName;
-        this.surName = surName;
-        this.age = age;
-        this.gender = gender;
+    constructor(firstName, surName, age, gender, email, allergies, id, table, seat){
+        if(arguments.length == 1){
+            //check if they are object for import
+        }else{
+            this.firstName = firstName || undefined;
+            this.surName = surName || undefined;;
+            this.age = age || undefined;;
+            this.gender = gender || undefined;;
+            this.email = email ||undefined;
+            this.allergies = allergies || [];
+            this.id = id || generateID();
+            this.table = table || undefined; //pointer
+            this.seat = seat || undefined;
+        }
+    }
+
+
+
+
+
+
+    getAllergies(){
+        return this.allergies;
+    }
+    getTable(){
+        return this.table;
+    }
+    getSeat(){
+        return this.seat;
+    }
+    export(){
+        return self.JSON;
     }
 }
--- app/static/js/ProjectClass.js
+++ app/static/js/ProjectClass.js
@@ -1,10 +1,12 @@
 class Project{
-    constructor(room, name, id, creationDate, creationUser){
+    constructor(room, name, id, allowedModerators,creationDate, creationUser){
         this.room = room || undefined;
         this.name = name || "Ditt Prosjekt";
         this.id = id || generateID();
         this.guests = [];
-        
+        this.allowedModerators = allowedModerators || [];
+        this.creationDate = creationDate;
+        this.creationUser = creationUser; // Pointer
     }
     
 }
